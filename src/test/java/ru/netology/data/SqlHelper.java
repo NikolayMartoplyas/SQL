43e2eb1882a9736eb3d56900a96d479fa7c98b31
Nikolay Martoplyas <kolyan94martoplyas@gmail.com>
2024-09-13 19:25:23 +0300

    initial commit

---
+++ src/test/java/ru/netology/data/SqlHelper.java
@@ -0,0 +1,39 @@
+package ru.netology.data;
+
+import lombok.SneakyThrows;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+
+public class SqlHelper {
+    private static final QueryRunner QUERY_RUNNER = new QueryRunner();
+
+    private SqlHelper(){
+    }
+    public static Connection getConn() throws SQLException{ //подключение к СУБД
+        return DriverManager.getConnection("app.url", "app", "pass");
+    }
+    @SneakyThrows
+    public static void cleanDatabase(){
+        var connection = getConn();
+        QUERY_RUNNER.execute(connection,"DELETE FROM users");
+        QUERY_RUNNER.execute(connection,"DELETE FROM cards");
+        QUERY_RUNNER.execute(connection,"DELETE FROM auth_codes");
+        QUERY_RUNNER.execute(connection,"DELETE FROM card_transactions");
+    }
+    @SneakyThrows
+    public static String getVerifaicationCode(){
+        var requestSQL = "SELECT code FROM auth_codes ORDER BY created DESC";
+        var conn = getConn();
+        return QUERY_RUNNER.query(conn, requestSQL, new ScalarHandler<>());
+    }
+    @SneakyThrows
+    public static void cleanAuthCode(){
+        var connection = getConn();
+        QUERY_RUNNER.execute(connection,"DELETE FROM auth_codes");
+    }
+
+}
---
+++ src/test/java/ru/netology/test/BankTest.java
@@ -0,0 +1,38 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SqlHelper;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SqlHelper.cleanAuthCode;
+import static ru.netology.data.SqlHelper.cleanDatabase;
+
+public class BankTest {
+    LoginPage loginPage;
+
+    @AfterEach
+    void tableСlearingAuthCode(){
+        cleanAuthCode();
+    }
+    @AfterAll
+    static void clearingAllTables(){
+        cleanDatabase();
+    }
+    @BeforeEach
+    void setUp(){
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+    @Test
+    void shouldSuccesFullLogin(){
+        var authInfo = DataHelper.getUser();
+        var verificatinPage = loginPage.validLogin(authInfo);
+        var verificationCode = SqlHelper.getVerifaicationCode();
+        verificatinPage.validCode(verificationCode);
+
+    }
+}
